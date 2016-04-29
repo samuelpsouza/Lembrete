e53d8940f2afc1d7b8dbcd6788fb1338208242f8
Samuel Souza <samuelsouza@alu.ufc.br>
2016-04-29 15:44:34 -0300

    main.js refactored

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,33 +1,29 @@
-angular.module('LembreteCtrl', []).controller('mainController', function($scope, $http){
+angular.module('LembreteCtrl', []).controller('mainController', function($scope, $http, Lembretes){
 	$scope.formData = {};
 
-	$http.get('/api/lembretes').success(function(data){
+	Lembretes.get().success(function(data){
 		$scope.lembretes = data;
-	}).error(function(data){
-		console.log('Error: ' + data);
 	});
 
 	$scope.createLembrete = function(){
-		$http.post('/api/lembretes', $scope.formData)
-		.success(function(data){
-			$scope.formData = {};
-			$scope.lembretes = data;
-		}).error(function(data){
-			console.log('Error: ' + data);
-		});
+		if(!$.isEmptyObject($scope.formData)){
+			Lembretes.create($scope.formData).success(function(data){
+				$scope.formData = {};
+				$scope.lembretes = data;
+			});
+		}
+	};
 
 	$scope.editLembrete = function(id){
-		$http.edit('/api/lembretes' + id)
+		Lembretes.edit()
 		.success(function(data){
 			$scope.lembretes = data;
-		}).error(function(data){
-			console.log('Error: ' + data);
-		});
+		};
 
-	$http.deleteLembrete = function(id){
-		$http.delete('/api/lembretes' + id).success(function(data){
+	$scope.deleteLembrete = function(id){
+		Lembretes.delete(id).success(function(data){
 			$scope.lembretes = data;
-		}).error(function(data){
-			console.log('Error: ' + data);
 		});
-};
+	};
+
+});
