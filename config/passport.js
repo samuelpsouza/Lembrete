993f67ee727fd2c0e797ade5b50a2b8ebd646437
Samuel Souza <samuelsouza@alu.ufc.br>
2016-05-02 09:18:37 -0300

    trying something new

---
+++ config/passport.js
@@ -0,0 +1,37 @@
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../app/models/model');
+
+module.exports = function(passport) {
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+    passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'username',
+        passwordField : 'passwd',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, username, password, done) { // callback with email and password from our form
+
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({ username: username }, function(err, user) {
+            if (err)
+                return done(err);
+            if (!user)
+                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+            if (!user.validPassword(password))
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+            return done(null, user);
+        });
+
+    }));
+};
