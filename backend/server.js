fc1daada6149a011b47fbb7ca795ad8d14419818
Samuel Souza <samuelsouza.contact@gmail.com>
2019-12-22 10:02:19 -0300

    refactoring app

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+const express = require('express');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
